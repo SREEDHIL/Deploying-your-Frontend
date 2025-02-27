f51d4486a4086671f846de5aea75a3e06e7592bd
SREEDHIL PAVISHANKER B <sreedhilpavishanker@gmail.com>
2025-02-27 10:31:16 +0530

    Add files via upload

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <div>
+      <h1>SREEDHIL PAVISHANKER B</h1>
+      <h3>96457XXXXX</h3>
+    </div>
+  )
+}
+
+export default Homepage
